cb02b0b6f4242a0d51f265d9a53ffb82c23285ff
Adrian Gasiorek <adrian.gasiorek@icloud.com>
2022-05-17 11:05:17 +0200

    Add getting href from clicked element

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict'
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   
@@ -11,14 +12,23 @@ const titleClickHandler = function(event){
       activeLink.classList.remove('active');
     }
   
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
   
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
   
     /* [DONE] remove class 'active' from all articles */
+      
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
   
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    
+    const linkHref = clickedElement.getAttribute('href');
+    console.log(linkHref);
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
